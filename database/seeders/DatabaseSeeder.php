1e322d0223483d6e77780d5d7715d6ff98f61f8a
jaroworkspace0-eng <jaroworkspace0@gmail.com>
2026-02-06 20:32:05 +0200

    -

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,12 +19,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'admin',
-        //     'email' => 'admin@example.com',
-        //     'password' => bcrypt('password'),
-        //     'role' => 'admin',
-        // ]);
+        User::factory()->create([
+            'name' => 'Karabo',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
         //    Client::create([
         //     'name' => 'jaro',
         //     'phone' => '0622219995',
