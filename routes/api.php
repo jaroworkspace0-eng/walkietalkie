1318009d000e821247961b4a462a7b52d05fbfcd
jaroworkspace0-eng <jaroworkspace0@gmail.com>
2026-02-08 21:16:11 +0200

    -

--- routes/api.php
+++ routes/api.php
@@ -111,11 +111,12 @@ Route::post('/user/update-status', [StatusController::class, 'updateStatus'])
 Route::prefix('v1')->name('api.')->group(function () {
     Route::apiResource('employees', EmployeeController::class);
     Route::resource('clients', ClientController::class);
-    Route::resource('channels', ChannelController::class);
+    // Route::resource('channels', ChannelController::class);
     Route::get('clients/list', [ClientController::class, 'clients']);
     // Route::get('channels/{channel}/units', [ChannelController::class, 'getUnits']);
 });
 
+Route::resource('channels', ChannelController::class);
 Route::get('channels/{channel}/units', [ChannelController::class, 'getUnits']);
 
 
